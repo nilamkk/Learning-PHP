02e0488ba50f0a8fb54c5beae1b017269eebc0e6
Nilam Kummar Kalita <nilamkk54438@gmail.com>
2021-10-23 20:36:33 +0530

    file write append added

--- theory/fileRead.php
+++ theory/fileRead.php
@@ -37,11 +37,50 @@ $fptr= fopen("assests/myFile.txt","r");
 if(!$fptr)
     die("Failed to open the file !!!");
 
-// reading character by character till * is encountered
+// reading file given the size of the characters
 $content=fread($fptr,filesize("assests/myFile.txt"));
 echo $content;
 
 fclose($fptr);
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+$fptr= fopen("assests/myFileW.txt","w");
+if(!$fptr)
+    die("Failed to open the file !!!");
+
+// writing to the file
+$content=fwrite($fptr,"I am writing these to myFileW.txt\n\n");
+echo $content;
+
+fclose($fptr);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+
+$fptr= fopen("assests/myFileW.txt","a");
+if(!$fptr)
+    die("Failed to open the file !!!");
+
+// appending to the file
+$content=fwrite($fptr,"I am appending these to myFileW.txt\n\n");
+echo $content;
+
+fclose($fptr);
+
+
+
+
+
+
+
+
 
 
 
